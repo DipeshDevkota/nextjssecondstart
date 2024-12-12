d78574bdbbc2035a644813ab9a1e0fe73124f80e
Dipesh <dipesh@example.com>
2024-12-12 14:19:09 +0545

    revalidate

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 
-const page = () => {
+
+
+
+type User ={
+  id:number;
+  name:string;
+  username: string;
+  email: string;
+};
+
+
+const page = async() => {
+
+  const data = await fetch("https://jsonplaceholder.typicode.com/users",{
+    next:{revalidate:3600},
+  });
+
+
+  const values:User[] = await data.json();
+  console.log("Values are ",values)
   return (
-    <div>
-      Register
-    </div>
+
+<main>
+  <h1>Blog Posts</h1>
+  <ul>
+    {values.map((value)=>(
+      <li key = {value.id}>{value.name}</li>
+    ))}
+  </ul>
+</main>
   )
 }
 
